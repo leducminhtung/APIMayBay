c2f03d0517612a5d108982f70a5b14d187c61284
Lê Đức Minh Tùng <69838173+leducminhtung@users.noreply.github.com>
2022-01-06 01:07:12 +0700

    z

---
+++ APIMayBay/Controllers/api/CangBayController.cs
@@ -0,0 +1,36 @@
+﻿using APIMayBay.Models;
+using Lib.Entity;
+using Lib.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace APIMayBay.Controllers.api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CangBayController : ControllerBase
+    {
+        private CangBayService cangBayService;
+        public CangBayController(CangBayService cangBayService)
+        {
+            this.cangBayService = cangBayService;
+        }
+
+        [HttpGet("get-cangbay")]
+        //[Authorize(Roles = "Admin,Guest")]
+        public async Task<ActionResult> GetCangBayList()
+        {
+            List<CangBayViewModel> cangBayList = cangBayService.GetCangBays();
+            return Ok(new { status = true, data = cangBayList });
+        }
+
+        
+    }
+
+}
